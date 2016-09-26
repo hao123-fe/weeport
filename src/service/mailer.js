26942e95f9a771bc1c7d4dd3b20325c2024bb990
Dafrok <o.o@mug.dog>
2016-09-26 11:52:42 +0800

    mailer service

--- src/service/mailer.js
+++ src/service/mailer.js
@@ -1,3 +1,27 @@
-// TODOS: auto send mail
-
 const nodemailer = require('nodemailer')
+
+export default function (options) {
+  var transporter = nodemailer.createTransport({
+    host: 'email.baidu.com',
+    port: 587,
+    auth: {
+      user: options.username,
+      pass: options.password
+    }
+  })
+
+  var mailOptions = {
+    from: `${options.username}@baidu.com`, // sender address
+    to: options.to, // list of receivers
+    cc: options.cc
+    subject: options.title, // Subject line
+    html: options.content // html body
+  }
+
+  transporter.sendMail(mailOptions, function(error, info){
+      if(error){
+          return console.log(error);
+      }
+      console.log('Message sent: ' + info.response);
+  });
+}
