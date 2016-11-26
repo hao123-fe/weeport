b58aff1c3b282939ed5134c959a041070015960f
Dafrok <o.o@mug.dog>
2016-11-26 15:43:49 +0800

    backup

--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -1,7 +1,32 @@
 <template lang="jade">
-section Dashboard
+section
+  n3-row
+    n3-column(:col="12")
+      div.report-header
+        h4 数据备份
+  n3-row
+    n3-column(:col="12")
+      n3-button(@click="backup") 数据备份
+  n3-row
+    n3-column(:col="12")
+      div.report-footer
 </template>
 <script>
+import clipboard from 'clipboard-js'
+import {n3Row, n3Column, n3Button} from 'N3-Components'
+
 export default {
+  components: {
+    n3Row,
+    n3Column,
+    n3Button
+  },
+  methods: {
+    backup () {
+      const ls = global.localStorage
+      const data = ls.getItem('projects')
+      clipboard.copy(data)
+    }
+  }
 }
 </script>
