850a1382474e6d145e64a5ad7037086a1e0f6268
Dafrok <o.o@mug.dog>
2017-07-23 01:31:08 +0800

    Update libs

--- src/lib/util.js
+++ src/lib/util.js
@@ -14,3 +14,11 @@ export const focusPrevInput = $el => {
     $prevEl.focus()
   }
 }
+
+export const week = date => {
+  date = date instanceof Date ? date : new Date()
+  var first = new Date(date.getFullYear(), 0, 1)
+  var diff = ((date - first) / 86400000)
+  var days = (diff + first.getDay())
+  return Math.floor(days / 7).toString()
+}
