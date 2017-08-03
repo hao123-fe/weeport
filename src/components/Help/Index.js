44b963013fdbe93e7163a83baed516ba7c35ec5a
Dafrok <o.o@mug.dog>
2017-08-03 15:22:13 +0800

    change github cornor z-index

--- src/components/Help/Index.js
+++ src/components/Help/Index.js
@@ -15,7 +15,7 @@ export default class Help extends React.Component {
     return <section className={'help'} style={{background: theme.acrylicTexture60.background}}>
       <a href={'https://github.com/hao123-fe/weeport'}>
         <img
-          style={{position: 'fixed', top: 0, right: 0, border: 0}}
+          style={{position: 'fixed', top: 0, right: 0, border: 0, zIndex: 1}}
           src={'https://camo.githubusercontent.com/a6677b08c955af8400f44c6298f40e7d19cc5b2d/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f677261795f3664366436642e706e67'}
           alt={'Fork me on GitHub'}
           data-canonical-src={'https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png'} />
