67085d43d552f8a0ec33a0250535586b23c23bc7
Dafrok <o.o@mug.dog>
2017-08-12 11:56:32 +0800

    修改状态字符串

--- src/lib/tasks.js
+++ src/lib/tasks.js
@@ -7,19 +7,19 @@ const color = {
 }
 
 export const tasks = {
-  PENDING: {text: '等待中', color: color.DEFAULT},
-  TODO: {text: '计划中', color: color.DEFAULT},
-  DOING: {text: '执行中', color: color.INFO},
-  DONE: {text: '完成', color: color.SUCCESS},
-  DELAY: {text: '已延期', color: color.WARNING},
-  CANCELED: {text: '已取消', color: color.DANGER}
+  Pending: {text: '等待中', color: color.DEFAULT},
+  Todo: {text: '计划中', color: color.DEFAULT},
+  Doing: {text: '执行中', color: color.INFO},
+  Done: {text: '完成', color: color.SUCCESS},
+  Delay: {text: '已延期', color: color.WARNING},
+  Canceled: {text: '已取消', color: color.DANGER}
 }
 
 export default [
-  'PENDING',
-  'TODO',
-  'DOING',
-  'DONE',
-  'DELAY',
-  'CANCELED'
+  'Pending',
+  'Todo',
+  'Doing',
+  'Done',
+  'Delay',
+  'Canceled'
 ]
